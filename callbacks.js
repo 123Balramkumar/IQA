cb742a33470159eea6e42101edd1d016dfe5fb38
123Balramkumar <balramkgg99@gmail.com>
2023-05-30 21:53:14 +0530

    call back completed

---
+++ callbacks.js
@@ -0,0 +1,27 @@
+// D--> A callback is a function that will be executed after another function gets executed
+// In javascript, functions are treated as first-class citizens, they can be used as an argument of another function,
+//  can be returned by another function, and can be used as a property of an object.
+
+// D-->A callback is a function that is passed as an argument to another function and is invoked or called back at a later time when a particular event or task is completed.
+
+function sayHello(){
+    console.log("Hello")
+}
+
+function sayHi(){
+    console.log("Hi..")
+
+}
+
+function add(n1,n2,callBack){
+    // sayHello()  --> We can call in this way but we have to call every time if the no of function 
+    // sayHi()  
+    console.log(n1+n2);
+    callBack()
+}
+
+
+add(5,10,sayHello);
+add(5,20,sayHi); 
+
+
