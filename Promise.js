be160c1df6c9b340cfbbc08adfe750e2ad990f35
Balram kumar <balramkgg99@gmail.com>
2023-05-31 12:56:32 +0530

    AsynAwait completed

--- Promise.js
+++ Promise.js
@@ -1,11 +1,21 @@
 // Promise is something that is achieved or completed in the future.
 // In JavaScript, a promise is an object and we use the promise constructor to initialize a promise.
 
-var data = new Promise (function(resolve,reject){
-    setTimeout(function(){
-        resolve("new state");
-    },2000)
-})
-data.then(function(val){
-    console.log(val)
-})
+var data = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve("new state");
+    }, 2000);
+  
+    setTimeout(function() {
+      reject("An error occurred");
+    }, 4000);
+  });
+  
+  data
+    .then(function(val) {
+      console.log(val);
+    })
+    .catch(function(error) {
+      console.log("Error:", error);
+    });
+  
