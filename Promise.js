8631eb1b6f9af539311489d6b8d8950b912c465e
123Balramkumar <balramkgg99@gmail.com>
2023-05-31 08:38:36 +0530

    promise done

---
+++ Promise.js
@@ -0,0 +1,11 @@
+// Promise is something that is achieved or completed in the future.
+// In JavaScript, a promise is an object and we use the promise constructor to initialize a promise.
+
+var data = new Promise (function(resolve,reject){
+    setTimeout(function(){
+        resolve("new state");
+    },2000)
+})
+data.then(function(val){
+    console.log(val)
+})
