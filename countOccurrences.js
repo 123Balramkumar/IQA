f83d2cda6fc63082b08f967ad2edfcb2d78dec35
Balram kumar <balramkgg99@gmail.com>
2023-05-31 14:57:23 +0530

    count Occurence done

---
+++ countOccurrences.js
@@ -0,0 +1,22 @@
+function countOccurrences(arr) {
+    const occurrences = {};
+    
+    for (let i = 0; i < arr.length; i++){
+      const num = arr[i];
+      
+      if (occurrences[num]) {
+        occurrences[num] += 1;
+      } else {
+        occurrences[num] = 1;
+      }
+    }
+    
+    return occurrences;
+  }
+  
+  const arr = [1, 3, 5, 3, 7, 5, 1, 3, 7, 7, 2];
+
+//const result = countOccurrences(arr);
+  
+  console.log(countOccurrences(arr));
+  
