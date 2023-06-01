29237300e89f1ad81e31a3832179fb086856cf0e
123Balramkumar <balramkgg99@gmail.com>
2023-06-01 11:21:31 +0530

    reverse satring

---
+++ ReverseString.js
@@ -0,0 +1,18 @@
+function reverseWords(str) {
+    // Split the string into an array of words
+    const words = str.split(' ');
+  
+    // Reverse the order of the words
+    const reversedWords = words.reverse();
+  
+    // Join the reversed words back into a string
+    const reversedString = reversedWords.join(' ');
+  
+    return reversedString;
+  }
+  
+  const str = "Hello, world! This is a sample sentence.";
+  const result = reverseWords(str);
+  
+  console.log(result);
+  
