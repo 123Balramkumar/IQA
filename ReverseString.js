a6232dd1746235ea85e9d497c4c6d13b63ce6f0c
123Balramkumar <balramkgg99@gmail.com>
2023-06-01 11:49:39 +0530

    Even number

--- ReverseString.js
+++ ReverseString.js
@@ -3,12 +3,15 @@ function reverseWords(str) {
     const words = str.split(' ');
   
     // Reverse the order of the words
+
     const reversedWords = words.reverse();
   
     // Join the reversed words back into a string
+
     const reversedString = reversedWords.join(' ');
   
     return reversedString;
+    
   }
   
   const str = "Hello, world! This is a sample sentence.";
