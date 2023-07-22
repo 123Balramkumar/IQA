b9319c440d061aca35b27cf8766da9d89ba55b65
Balram kumar <balramkgg99@gmail.com>
2023-07-22 18:46:35 +0530

    count all those  name which have "a" in them

---
+++ String/RemoveCharacterFromString.js
@@ -0,0 +1,12 @@
+//remove b from the given String var 
+
+var name = "balram"
+var outPut =" "
+
+for(let i=0;i<name.length;i++){
+    if(name[i]== "a"){
+        continue;
+    }
+    outPut = outPut+name[i];
+}
+console.log(outPut);
