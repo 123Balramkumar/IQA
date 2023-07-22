926dc0272c0f016061b223f1dcf2dc2ca15fd64e
Balram kumar <balramkgg99@gmail.com>
2023-07-22 18:46:06 +0530

    count all those  name which have a in them

---
+++ Array/countTheName.js
@@ -0,0 +1,22 @@
+//count all those  name which have "a" in them
+
+var names = ["Arun", "sunil", "deepak", "sumit","khushi","Balram"]
+var count = 0;
+
+for(let i=0;i<names.length;i++){
+    var currentName = names[i];
+
+    for(let j=0;j<currentName.length;j++){
+
+        if(currentName[j]==="a" || currentName[j]==="A"){
+            count++;
+          
+            console.log(currentName)
+            break;
+        }
+    }
+}
+
+console.log(count)
+
+
